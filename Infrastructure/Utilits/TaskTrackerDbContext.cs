c4489d0d8422b9ec232bdc568f636ce0964e50de
Артём Миков <artem.mikov.2003@mail.ru>
2024-03-06 21:33:06 +0500

    Data project

---
+++ Infrastructure/Utilits/TaskTrackerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Utilits;
+
+public partial class TaskTrackerDbContext : DbContext
+{
+    public TaskTrackerDbContext()
+    {
+    }
+
+    public TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=MSI;Database=TaskTrackerDB;Trusted_Connection=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
